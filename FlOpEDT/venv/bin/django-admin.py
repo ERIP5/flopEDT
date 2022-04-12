5efd0318ebf838366f7ae41d4a70a21f528b181f
WeppeAlexis <weppe.alexis@gmail.com>
2022-04-12 15:08:44 +0200

    debut script et tentative extends

---
+++ FlOpEDT/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/Etudiant/flop-edt-flop-scheduler-sujet-1/FlOpEDT/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
