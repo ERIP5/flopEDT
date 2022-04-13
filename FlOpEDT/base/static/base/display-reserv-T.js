003c7ffe6aec2705e920206993ecc4ce17a638aa
nico2400 <nicolas042022@gmail.com>
2022-04-13 13:46:51 +0200

    add display_date

--- FlOpEDT/base/static/base/display-reserv-T.js
+++ FlOpEDT/base/static/base/display-reserv-T.js
@@ -1,12 +1,23 @@
+
+
+
 var days_width = 150
-var days_heigth = 1000
-var room_width = 100
+var days_height = 1000
 var days = [{num: 0, ref: "m", name: "Lundi"},
             {num: 1, ref: "tu", name: "Mardi"},
             {num: 2, ref: "w", name: "Mercredi"},
             {num: 3, ref: "th", name: "Jeudi"},
             {num: 4, ref: "f", name: "Vendredi"}] ;
 
+
+var room_width = 100
+
+var date_width = days_width*5
+var date_height = 50
+var date_margtop = 20
+
+var room =[{}]
+var date =[{}]
 /**********************
 *gestion des variables*
 **********************/
@@ -16,27 +27,42 @@ function days_x(day){
     return days_width*(day.num)+room_width}
 
 function days_y(){
-    return 0
+    return date_height+date_margtop
 }
 
 /**********
 *affichage*
 **********/
-
-function display_room(){
+function display_date(){
 d3.select("svg")
-  .selectAll("rect_date")
-  .data(days)
+  .selectAll("rect")
+  .data(date)
   .enter()
   .append("rect")
   .attr("class","rect_date")
   .attr("fill","none")
   .attr("stroke","black")
   .attr("stroke-width",5)
+  .attr("x",room_width)
+  .attr("y",date_margtop)
+  .attr("width",date_width)
+  .attr("height",date_height)
+}
+
+function display_room(){
+d3.select("svg")
+  .selectAll("rect_room")
+  .data(room)
+  .enter()
+  .append("rect")
+  .attr("class","rect_room")
+  .attr("fill","none")
+  .attr("stroke","black")
+  .attr("stroke-width",5)
   .attr("x",0)
-  .attr("y",0)
+  .attr("y",days_y)
   .attr("width",room_width)
-  .attr("height",days_heigth)
+  .attr("height",days_height)
   }
 
 function display_grid(){
@@ -52,10 +78,11 @@ d3.select("svg")
   .attr("x",days_x)
   .attr("y",days_y)
   .attr("width",days_width)
-  .attr("height",days_heigth)
+  .attr("height",days_height)
   }
 d3.select("svg")
-        .attr("height", days_heigth)
+        .attr("height", days_height)
         .attr("width", 1000) ;
+display_date();
 display_room();
 display_grid();
