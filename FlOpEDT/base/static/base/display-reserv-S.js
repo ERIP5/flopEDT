407ad31864a820ae4301a6ad55dea7c0c244e4a8
WeppeAlexis <weppe.alexis@gmail.com>
2022-04-15 13:18:43 +0200

    the hours are finished

--- FlOpEDT/base/static/base/display-reserv-S.js
+++ FlOpEDT/base/static/base/display-reserv-S.js
@@ -1,18 +1,32 @@
-var days_width = 150
-var days_height = 1440
-
-var horaire_width = 150
-
-var date_width = days_width*5
 var date_height = 50
 var date_margin_top = 20
 
 var echelle_width = 50
-var echelle_height = 1440
 var echelle_y = cac_echelle_y()
 var echelle_x = 100
+var echelle_start_time = 8*60+15 //495
+var echelle_end_time_day = 18*60+45 //1005
+var echelle_day_start_hour = ~~(echelle_start_time/60)
+var echelle_day_start_min = echelle_start_time%60
+var echelle_day_start_concat = echelle_day_start_hour + "h" + echelle_day_start_min
+var echelle_end_time_day_hour = ~~(echelle_end_time_day/60)
+var echelle_end_time_day_min = echelle_end_time_day%60
+var echelle_end_time_day_concat = echelle_end_time_day_hour + "h" + echelle_end_time_day_min
+var echelle_height = days_duration()
+
+var days_width = 150
+var days_height = days_duration()
+
+var horaire_width = 150
+
+var date_width = days_width*5
+
 
 var each_hour_y = days_y()
+var each_hour_yy = days_y()
+
+var each_time_text = 9
+
 
 /*
 // a changer
@@ -55,8 +69,8 @@ var each_reserv = [
 *gestion des variables*
 **********************/
 
-function course_duration(){
-    return (each_reserv.end-each_reserv.start)*2}
+function days_duration(){
+    return (echelle_end_time_day-echelle_start_time)}
 
 function days_x(day){
     return days_width*(day.num)+horaire_width}
@@ -70,19 +84,47 @@ function getday(day){
 }
 
 function cac_echelle_y(){
-    console.log(date_height+date_margin_top);
     return date_height+date_margin_top
 }
 
-function display_text(){
-    for (let i =0; i++;i<19)
-    return 8
+function display_echelle_start_time(){
+    return echelle_start_time
 }
 
-function each_text_posy(){
+function display_echelle_end_time_day(){
+    return echelle_end_time_day
+}
+
+function echelle_each_time_quaranteCinq(){
     var y = each_hour_y;
-    each_hour_y += 120; //y+120 ou un truc du genre
+    each_hour_y += 45;
     return y;
+}
+
+function echelle_each_time_quaranteCinq_h(){
+    var yy = each_hour_yy;
+    each_hour_yy += 45;
+    return yy;
+}
+
+function echelle_each_time_soixante(){
+    var y = each_hour_y;
+    each_hour_y += 60;
+    return y;
+}
+
+function echelle_each_time_soixante_h(){
+    var yy = each_hour_yy;
+    each_hour_yy += 60;
+    //console.log(yy)
+    return yy;
+}
+
+function echelle_each_time_text(){
+    var yyy = each_time_text;
+    each_time_text += 1;
+    console.log(yyy);
+    return yyy;
 }
 
  /***************
@@ -147,9 +189,148 @@ c_echelle = d3.select(".echelles")
 
   c_echelle
   .append("text")
-  .text(display_text)
-  .attr("x",120)
-  .attr("y", each_text_posy)
+  .text(echelle_day_start_concat)
+  .attr("x",110)
+  .attr("y", echelle_each_time_quaranteCinq)
+
+  c_echelle
+  .append("text")
+  .text("")
+  .attr("x",130)
+  .attr("y", echelle_each_time_quaranteCinq_h)
+
+  c_echelle
+  .append("text")
+  .text(echelle_each_time_text)
+  .attr("x",110)
+  .attr("y", echelle_each_time_soixante)
+
+  c_echelle
+  .append("text")
+  .text("h")
+  .attr("x",130)
+  .attr("y", echelle_each_time_soixante_h)
+
+  c_echelle
+  .append("text")
+  .text(echelle_each_time_text)
+  .attr("x",110)
+  .attr("y", echelle_each_time_soixante)
+
+  c_echelle
+  .append("text")
+  .text("h")
+  .attr("x",130)
+  .attr("y", echelle_each_time_soixante_h)
+
+  c_echelle
+  .append("text")
+  .text(echelle_each_time_text)
+  .attr("x",110)
+  .attr("y", echelle_each_time_soixante)
+
+  c_echelle
+  .append("text")
+  .text("h")
+  .attr("x",130)
+  .attr("y", echelle_each_time_soixante_h)
+
+  c_echelle
+  .append("text")
+  .text(echelle_each_time_text)
+  .attr("x",110)
+  .attr("y", echelle_each_time_soixante)
+
+  c_echelle
+  .append("text")
+  .text("h")
+  .attr("x",130)
+  .attr("y", echelle_each_time_soixante_h)
+
+  c_echelle
+  .append("text")
+  .text(echelle_each_time_text)
+  .attr("x",110)
+  .attr("y", echelle_each_time_soixante)
+
+  c_echelle
+  .append("text")
+  .text("h")
+  .attr("x",130)
+  .attr("y", echelle_each_time_soixante_h)
+
+  c_echelle
+  .append("text")
+  .text(echelle_each_time_text)
+  .attr("x",110)
+  .attr("y", echelle_each_time_soixante)
+
+  c_echelle
+  .append("text")
+  .text("h")
+  .attr("x",130)
+  .attr("y", echelle_each_time_soixante_h)
+
+  c_echelle
+  .append("text")
+  .text(echelle_each_time_text)
+  .attr("x",110)
+  .attr("y", echelle_each_time_soixante)
+
+  c_echelle
+  .append("text")
+  .text("h")
+  .attr("x",130)
+  .attr("y", echelle_each_time_soixante_h)
+
+  c_echelle
+  .append("text")
+  .text(echelle_each_time_text)
+  .attr("x",110)
+  .attr("y", echelle_each_time_soixante)
+
+  c_echelle
+  .append("text")
+  .text("h")
+  .attr("x",130)
+  .attr("y", echelle_each_time_soixante_h)
+
+  c_echelle
+  .append("text")
+  .text(echelle_each_time_text)
+  .attr("x",110)
+  .attr("y", echelle_each_time_soixante)
+
+  c_echelle
+  .append("text")
+  .text("h")
+  .attr("x",130)
+  .attr("y", echelle_each_time_soixante_h)
+
+  c_echelle
+  .append("text")
+  .text(echelle_each_time_text)
+  .attr("x",110)
+  .attr("y", echelle_each_time_quaranteCinq)
+
+  c_echelle
+  .append("text")
+  .text("h")
+  .attr("x",130)
+  .attr("y", echelle_each_time_quaranteCinq_h)
+
+  c_echelle
+  .append("text")
+  .text(echelle_end_time_day_concat)
+  .attr("x",105)
+  .attr("y", echelle_each_time_quaranteCinq)
+
+    c_echelle
+  .append("text")
+  .text("")
+  .attr("x",130)
+  .attr("y", echelle_each_time_quaranteCinq_h)
+
 }
 
 /*******
@@ -163,7 +344,11 @@ d3.select("svg")
         .attr("height", 1600)
         .attr("width", 1600) ;
 
-// 60min*12h(8h->19h) = 720min *2= 1440pix
-// 12h = 1400pix
-// 1h = 60min = 120pix
-// 1min = 2pix
+/* 60min*12h(8h->19h) = 720min *2= 1440pix
+ 12h = 1400pix
+ 1h = 60min = 120pix
+ 1min = 2pix */
+
+/*  let start_time = 8*60+15
+    let end_time_day = 18*60+45 */
+
