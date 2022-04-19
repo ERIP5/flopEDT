2e83738c37058034334ec59af4c3af0ff8289fcc
WeppeAlexis <weppe.alexis@gmail.com>
2022-04-19 16:38:59 +0200

    upgrade echelle and trying to use another data

--- FlOpEDT/base/static/base/display-reserv-S.js
+++ FlOpEDT/base/static/base/display-reserv-S.js
@@ -5,13 +5,13 @@ var echelle_width = 50
 var echelle_y = cac_echelle_y()
 var echelle_x = 100
 var echelle_start_time = 8*60+15 //495
-var echelle_end_time_day = 18*60+45 //1005
-var echelle_day_start_hour = ~~(echelle_start_time/60)
-var echelle_day_start_min = echelle_start_time%60
-var echelle_day_start_concat = echelle_day_start_hour + "h" + echelle_day_start_min
-var echelle_end_time_day_hour = ~~(echelle_end_time_day/60)
-var echelle_end_time_day_min = echelle_end_time_day%60
-var echelle_end_time_day_concat = echelle_end_time_day_hour + "h" + echelle_end_time_day_min
+var echelle_day_start_hour = Math.floor(echelle_start_time/60)+1
+var echelle_day_start_hour_concat = echelle_day_start_hour + "h"
+var echelle_end_time_day = 18*60+45 //1125
+var echelle_end_time_day_hour = Math.floor(echelle_end_time_day/60)
+var echelle_end_time_day_hour_concat = echelle_end_time_day_hour+ "h"
+var echelle_current_hour = echelle_day_start_hour
+
 var echelle_height = days_duration()
 
 var days_width = 150
@@ -24,7 +24,6 @@ var date_width = days_width*5
 var each_hour_y = days_y()
 var each_hour_yy = days_y()
 
-var each_time_text = 9
 
 var room =[{}]
 
@@ -32,7 +31,7 @@ var date =[{}]
 
 var echelle =[{}]
 
-var current_room = "B102"
+var current_room = "414"
 
 var days = [{num: 0, ref: "m", name: "Lundi"},
             {num: 1, ref: "tu", name: "Mardi"},
@@ -51,6 +50,7 @@ var each_reserv = [
 { "day" : "th", "room" : "B112", "start": 480, "end" : 540, "name" : "LN" }
 ]*/
 
+/*
 let courses = [
   {
     "id_course": 137455,
@@ -136,12 +136,63 @@ let courses = [
     "promo": 0,
     "from_transversal": null
   }]
+  */
 
+let rooms = [
+  {"name": "414", "display":true, "type":"A","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[{
+    "id_course": 137457,
+    "department": 'INFO',
+    "mod": "ExploJAVA",
+    "c_type": "Projet",
+    "day": "w",
+    "start": 585,
+    "duration": 85,
+    "room": "414",
+    "room_type": "M",
+    "display": true,
+    "id_visio": -1,
+    "graded": false,
+    "color_bg": "#ec4dd8",
+    "color_txt": "#000000",
+    "tutor": "PSE",
+    "supp_tutors" : [],
+    "group": "4B",
+    "promo": 0,
+    "from_transversal": null
+  },{
+    "id_course": 137458,
+    "department": 'INFO',
+    "mod": "ExplBD",
+    "c_type": "Projet",
+    "day": "w",
+    "start": 700,
+    "duration": 85,
+    "room": "414",
+    "room_type": "M",
+    "display": true,
+    "id_visio": -1,
+    "graded": false,
+    "color_bg": "#ec4dd7",
+    "color_txt": "#000000",
+    "tutor": "PSE",
+    "supp_tutors" : [],
+    "group": "4B",
+    "promo": 0,
+    "from_transversal": null
+  },], 'th':[],'f':[]}},
+  {"name": "G21", "display":false, "type":"A","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
+  {"name": "G26", "display":false, "type":"A","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
+  {"name": "E209", "display":true, "type":"A","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
+  {"name": "B111", "display":true, "type":"A","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
+  {"name": "B002", "display":true, "type":"A","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
+  {"name": "B203", "display":true, "type":"A","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]}}
+]
 
 
 /**********************
 *gestion des variables*
 **********************/
+
 
 function days_duration(){
     return (echelle_end_time_day-echelle_start_time)}
@@ -161,122 +212,90 @@ function cac_echelle_y(){
     return date_height+date_margin_top
 }
 
+function echelle_current_hour_incr() {
+    var h = echelle_current_hour+1
+    echelle_current_hour += 1
+    return h
+}
+
+function echelle_current_hour_concat(){
+    var hhh = echelle_current_hour_incr()+ "h"
+    return hhh
+}
+
+
 function display_echelle_start_time(){
-    return echelle_start_time
+    return echelle_start_time_hour_concat
 }
 
 function display_echelle_end_time_day(){
-    return echelle_end_time_day
-}
-
-function echelle_each_time_quaranteCinq(){
-    var y = each_hour_y;
-    each_hour_y += 45;
-    return y;
-}
-
-function echelle_each_time_quaranteCinq_h(){
-    var yy = each_hour_yy;
-    each_hour_yy += 45;
-    return yy;
+    return echelle_end_time_day_hour_concat
 }
 
 function echelle_each_time_soixante(){
+console.log((echelle_start_time+60)-echelle_start_time-echelle_start_time%60)
     var y = each_hour_y;
+    if (y == 70) {
+    //console.log("if "+each_hour_y)
+    each_hour_y += ((echelle_start_time+60)-echelle_start_time-echelle_start_time%60);
+    //console.log("if "+each_hour_y)
+    return y    }
     each_hour_y += 60;
-    //console.log(y)
+    //console.log(each_hour_y+" else")
     return y;
 }
 
-function echelle_each_time_soixante_h(){
-    var yy = each_hour_yy;
-    each_hour_yy += 60;
-    //console.log(yy)
-    return yy;
-}
-
-function echelle_each_time_text(){
-    var yyy = each_time_text;
-    each_time_text += 1;
-    ///console.log(yyy);
-    return yyy;
-}
-
-/*
-function reserv_dispo(){
-    //console.log("reserv_dispo")
-    for (i=0;i<courses.length;i++) {
-        console.log("--------------")
-        if (current_room == courses[i].room) {
-            //console.log("if")
-
-            console.log("id_course")
-            //console.log(courses[i].id_course)
-            res_id_course = courses[i].id_course
-            //console.log(res_id_course)
-
-            console.log("day")
-            //console.log(courses[i].day)
-            res_day = courses[i].day
-            //console.log(res_day)
-
-            console.log("start")
-            //console.log(courses[i].start)
-            res_start = courses[i].start
-            //console.log(res_start)
-
-            console.log("duration")
-            //console.log(courses[i].duration)
-            res_duration = courses[i].duration
-            //console.log(res_duration)
-            }
-        else {
-            //console.log("else")
-            console.log(courses[i].id_course+" is not in the right room")
-            }
-    }
-}
-*/
-
 function course_x(day){
-    //console.log(courses[i].day)
-    if (courses[i].day == "m"){
+    for (day of days){
+    console.log(day.day)
+    if (day[day.ref] == "w") {
+    console.log("if w")
+    return days_width*(2)+horaire_width
+    }
+
+    //console.log(rooms[i].courses["w"])
+    /*if (rooms[i].courses["m"] == "m"){
         return days_width*(0)+horaire_width
         }
-    if (courses[i].day == "tu"){
+    if (rooms[i].courses["tu"] == "tu"){
         return days_width*(1)+horaire_width
         }
-    if (courses[i].day == "w"){
+    if (rooms[i].courses["w"] == "w"){
         return days_width*(2)+horaire_width
         }
-    if (courses[i].day == "th"){
+    if (rooms[i].courses["th"] == "th"){
         return days_width*(3)+horaire_width
         }
-    if (courses[i].day == "f"){
+    if (rooms[i].courses["f"] == "f"){
         return days_width*(4)+horaire_width
+        }*/
+
+    /*
+        if (day.ref == course[day.ref]){
+        console.log(2)
+        console.log(days_width*element["num"]+room_width)
+            return days_width*element["num"]+room_width
         }
+    */
     }
+}
 
 function course_y(){
-    return date_height+date_margin_top+courses[i].start-echelle_start_time
+//console.log("course_y")
+//console.log(/*date_height+date_margin_top-echelle_start_time+*/rooms[0].courses)
+    return date_height+date_margin_top+rooms[i].start-echelle_start_time
 }
 
 function course_y_text_module(){
-    rr = date_height+date_margin_top
-    console.log(rr)
-    return date_height+date_margin_top+courses[i].start-echelle_start_time+10
+    return date_height+date_margin_top+rooms[i].start-echelle_start_time+10
 }
 
 function course_y_text_c_type(){
-    rr = date_height+date_margin_top
-    console.log(rr)
-    return date_height+date_margin_top+courses[i].start-echelle_start_time+25
+    return date_height+date_margin_top+rooms[i].start-echelle_start_time+25
 }
 
 function course_y_text_tutor(){
-    rr = date_height+date_margin_top
-    console.log(rr)
-    return date_height+date_margin_top+courses[i].start-echelle_start_time+40
+    return date_height+date_margin_top+rooms[i].start-echelle_start_time+40
 }
 
 
@@ -342,195 +361,90 @@ c_echelle = d3.select(".echelles")
 
   c_echelle
   .append("text")
-  .text(echelle_day_start_concat)
-  .attr("x",110)
-  .attr("y", echelle_each_time_quaranteCinq)
-
-  c_echelle
-  .append("text")
   .text("")
-  .attr("x",130)
-  .attr("y", echelle_each_time_quaranteCinq_h)
-
-  c_echelle
-  .append("text")
-  .text(echelle_each_time_text)
   .attr("x",110)
   .attr("y", echelle_each_time_soixante)
 
   c_echelle
   .append("text")
-  .text("h")
-  .attr("x",130)
-  .attr("y", echelle_each_time_soixante_h)
-
-  c_echelle
-  .append("text")
-  .text(echelle_each_time_text)
+  .text(echelle_day_start_hour_concat)
   .attr("x",110)
   .attr("y", echelle_each_time_soixante)
 
-  c_echelle
-  .append("text")
-  .text("h")
-  .attr("x",130)
-  .attr("y", echelle_each_time_soixante_h)
 
+  for (hh = echelle_current_hour+1; hh<echelle_end_time_day_hour; hh++){
   c_echelle
   .append("text")
-  .text(echelle_each_time_text)
+  .text(echelle_current_hour_concat)
   .attr("x",110)
   .attr("y", echelle_each_time_soixante)
+}
 
   c_echelle
   .append("text")
-  .text("h")
-  .attr("x",130)
-  .attr("y", echelle_each_time_soixante_h)
-
-  c_echelle
-  .append("text")
-  .text(echelle_each_time_text)
+  .text(echelle_end_time_day_hour_concat)
   .attr("x",110)
   .attr("y", echelle_each_time_soixante)
-
-  c_echelle
-  .append("text")
-  .text("h")
-  .attr("x",130)
-  .attr("y", echelle_each_time_soixante_h)
-
-  c_echelle
-  .append("text")
-  .text(echelle_each_time_text)
-  .attr("x",110)
-  .attr("y", echelle_each_time_soixante)
-
-  c_echelle
-  .append("text")
-  .text("h")
-  .attr("x",130)
-  .attr("y", echelle_each_time_soixante_h)
-
-  c_echelle
-  .append("text")
-  .text(echelle_each_time_text)
-  .attr("x",110)
-  .attr("y", echelle_each_time_soixante)
-
-  c_echelle
-  .append("text")
-  .text("h")
-  .attr("x",130)
-  .attr("y", echelle_each_time_soixante_h)
-
-  c_echelle
-  .append("text")
-  .text(echelle_each_time_text)
-  .attr("x",110)
-  .attr("y", echelle_each_time_soixante)
-
-  c_echelle
-  .append("text")
-  .text("h")
-  .attr("x",130)
-  .attr("y", echelle_each_time_soixante_h)
-
-  c_echelle
-  .append("text")
-  .text(echelle_each_time_text)
-  .attr("x",110)
-  .attr("y", echelle_each_time_soixante)
-
-  c_echelle
-  .append("text")
-  .text("h")
-  .attr("x",130)
-  .attr("y", echelle_each_time_soixante_h)
-
-  c_echelle
-  .append("text")
-  .text(echelle_each_time_text)
-  .attr("x",110)
-  .attr("y", echelle_each_time_soixante)
-
-  c_echelle
-  .append("text")
-  .text("h")
-  .attr("x",130)
-  .attr("y", echelle_each_time_soixante_h)
-
-  c_echelle
-  .append("text")
-  .text(echelle_each_time_text)
-  .attr("x",110)
-  .attr("y", echelle_each_time_quaranteCinq)
-
-  c_echelle
-  .append("text")
-  .text("h")
-  .attr("x",130)
-  .attr("y", echelle_each_time_quaranteCinq_h)
-
-  c_echelle
-  .append("text")
-  .text(echelle_end_time_day_concat)
-  .attr("x",105)
-  .attr("y", echelle_each_time_quaranteCinq)
-
-    c_echelle
-  .append("text")
-  .text("")
-  .attr("x",130)
-  .attr("y", echelle_each_time_quaranteCinq_h)
 
 }
 
+/*
 function display_reservation(){
     c_reservations = d3.select(".reservations")
     .selectAll("rect")
-    .data(courses)
+    .data(rooms)
     .enter();
 
-    for (i=0; i<courses.length;i++){
-        if (courses[i].display == true) {
-            if (current_room == courses[i].room) {
+
+    //console.log(rooms.length)
+    for (i=0; i<rooms.length;i++){
+        if (rooms[i].display == true) {
+            //console.log(rooms[i].name)
+            if (current_room == rooms[i].name) {
+                for day in days:
+                    x = course_x(day)
+                    days_courses = room_object.courses[day.ref]
+                    for course in days_courses:
+                        y =
+
+                //console.log(rooms[i].name)
                 c_reservations
                     .append("rect")
                     .attr("class", "rect_grid")
-                    .attr("fill",courses[i].color_bg)
+                    .attr("fill",rooms[i].color_bg)
                     .attr("stroke", "black")
-                    .attr("x", course_x(courses[i].day))
+                    .attr("x", course_x(rooms[i].courses))
                     .attr("y", course_y)
                     .attr("width", days_width)
-                    .attr("height", courses[i].duration)
+                    .attr("height", rooms[i].duration)
 
                 c_reservations
                     .append("text")
-                    .text(courses[i].mod)
-                    .attr("x", course_x(courses[i].day)+50)
+                    .text(rooms[i].mod)
+                    .attr("x", course_x(rooms[i].day)+50)
                     .attr("y", course_y_text_module)
-                    .attr("style_color", courses[i].color_txt)
+                    //.attr("style_color", rooms[i].color_txt)
 
                 c_reservations
                     .append("text")
-                    .text(courses[i].c_type)
-                    .attr("x", course_x(courses[i].day)+50)
+                    .text(rooms[i].c_type)
+                    .attr("x", course_x(rooms[i].day)+50)
                     .attr("y", course_y_text_c_type)
-                    .attr("style.color", courses[i].color_txt)
+                    //.attr("style.color", rooms[i].color_txt)
 
                 c_reservations
                     .append("text")
-                    .text(courses[i].tutor)
-                    .attr("x", course_x(courses[i].day)+50)
+                    .text(rooms[i].tutor)
+                    .attr("x", course_x(rooms[i].day)+50)
                     .attr("y", course_y_text_tutor)
-                    .attr("text_color", courses[i].color_txt)
+                    //.attr("text_color", rooms[i].color_txt)
 
 
             }
         }
     }
 }
+*/
 
 /*******
 *display*
@@ -538,18 +452,8 @@ function display_reservation(){
 display_date();
 display_grid();
 display_echelle();
-display_reservation();
-//reserv_dispo();
+//display_reservation();
 
 d3.select("svg")
         .attr("height", 1600)
         .attr("width", 1600) ;
-
-/* 60min*12h(8h->19h) = 720min *2= 1440pix
- 12h = 1400pix
- 1h = 60min = 120pix
- 1min = 2pix */
-
-/*  let start_time = 8*60+15
-    let end_time_day = 18*60+45 */
-
