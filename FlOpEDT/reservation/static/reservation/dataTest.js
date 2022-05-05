491fdf2245bd6c07b31641f64ed1f8b8839417b6
nico2400 <Mathieu.04>
2022-05-05 10:38:34 +0200

    add projector for each room

--- FlOpEDT/reservation/static/reservation/dataTest.js
+++ FlOpEDT/reservation/static/reservation/dataTest.js
@@ -16,7 +16,7 @@ let days = [{num: 0, ref: "m", name: "Lundi"},
 
 
 let rooms = [
-  {"name": "B112", "display":true, "type":"A","y":0, "height":0, 'courses':{'m':[{
+  {"name": "B112", "display":true, "type":"A","y":0, "height":0, "projector":true, 'courses':{'m':[{
     "id_course": 137455,
     "department": 'INFO',
     "mod": "ExplBD",
@@ -111,13 +111,13 @@ let rooms = [
     "type": "type",
     "key":true
 }], 'th':[],'f':[]}},
-  {"name": "G21", "display":false, "type":"A","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
-  {"name": "G26", "display":false, "type":"A","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
-  {"name": "E209", "display":true, "type":"A","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
-  {"name": "B111", "display":true, "type":"B","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
-  {"name": "B002", "display":true, "type":"B","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
-  {"name": "B203", "display":true, "type":"B","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
-  {"name": "Amphi1", "display":true, "type":"amphi","y":0, "height":0, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}}
+  {"name": "G21", "display":false, "type":"A","y":0, "height":0, "projector":true, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
+  {"name": "G26", "display":false, "type":"A","y":0, "height":0, "projector":true, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
+  {"name": "E209", "display":true, "type":"A","y":0, "height":0, "projector":true, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
+  {"name": "B111", "display":true, "type":"B","y":0, "height":0, "projector":true, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
+  {"name": "B002", "display":true, "type":"B","y":0, "height":0, "projector":true, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
+  {"name": "B203", "display":true, "type":"B","y":0, "height":0, "projector":true, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}},
+  {"name": "Amphi1", "display":true, "type":"amphi","y":0, "height":0, "projector":true, 'courses':{'m':[],'tu':[],'w':[], 'th':[],'f':[]},'booking' :{'m':[],'tu':[],'w':[], 'th':[],'f':[]}}
 ]
 
 
