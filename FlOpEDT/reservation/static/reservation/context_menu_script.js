a9712b5d3fb4d89a8842126728bab6b47b6f48a5
WeppeAlexis <weppe.alexis@gmail.com>
2022-05-05 11:20:07 +0200

    save for menu

--- FlOpEDT/reservation/static/reservation/context_menu_script.js
+++ FlOpEDT/reservation/static/reservation/context_menu_script.js
@@ -2,14 +2,14 @@ document.onclick = hideMenu;
 document.oncontextmenu = rightClick;
 
 function hideMenu() {
-			document.getElementById("contextMenu").style.display = "none"
+			document.getElementById("contextMenu").style.visibility = "hidden"
 			//console.log("hide")
 		}
 
 function rightClick(e) {
     e.preventDefault()
 
-    if (document.getElementById("contextMenu").style.display == 'block') {
+    if (document.getElementById("contextMenu").style.visibility == 'visible') {
         console.log("if")
         hideMenu();
         }
@@ -18,7 +18,7 @@ function rightClick(e) {
         var menu = document.createElement("contextMenu")
         menu.id = "contextMenu"
 
-        menu.style.display = 'block';
+        menu.style.visibility = 'visible';
         menu.style.left = e.pageX + "px";
         menu.style.top = e.pageY + "px";
 
