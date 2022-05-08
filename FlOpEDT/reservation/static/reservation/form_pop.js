8a358e7c5ad6567e5cffc498045576cc91bad87a
WeppeAlexis <weppe.alexis@gmail.com>
2022-05-08 19:41:31 +0200

    before week 5

---
+++ FlOpEDT/reservation/static/reservation/form_pop.js
@@ -0,0 +1,8 @@
+var a = 3
+
+function openForm() {
+document.getElementById("popupForm").style.display = "block";
+}
+function closeForm() {
+document.getElementById("popupForm").style.display = "none";
+}
