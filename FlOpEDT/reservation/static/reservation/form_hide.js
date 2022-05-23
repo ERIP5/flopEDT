02b6d310fe304d0148eeda4f6fe2401aa0c15e2f
WeppeAlexis <weppe.alexis@gmail.com>
2022-05-23 11:39:45 +0200

    form hide

--- FlOpEDT/reservation/static/reservation/form_hide.js
+++ FlOpEDT/reservation/static/reservation/form_hide.js
@@ -1,31 +1,60 @@
-/*
-a=document.getElementById("id_bm_x_choice")
-a.hidden=true
-a.hidden=false
-a.parentElement.hidden=true
+function errorHide() {
+    document.getElementsByClassName("errorlist")
 
-laisser hidden le select periodicity
-
-
-
-hide le 2eme formulaire
-if has_periodicity == true
-    show : start_time, duration, periodicity
-
-    if by week
-
-
-    if each month
-
-
-    if by month
-*/
-var truck = 3
-
-function first_hide() {
-    document.getElementById("id_periodicity_type").parentElement.hidden=true
 }
 
 function mainHide() {
-    first_hide()
+    // must be hidden
+    document.getElementById("id_periodicity").parentElement.hidden=true
+
+    // hide all at the start
+    document.getElementById("id_periodicity_type").parentElement.hidden=true
+    document.getElementById("id_start").parentElement.hidden=true
+    document.getElementById("id_end").parentElement.hidden=true
+    document.getElementById("id_bw_weekdays").parentElement.hidden=true
+    document.getElementById("id_bw_weeks_nb").parentElement.hidden=true
+    document.getElementById("id_bm_x_choice").parentElement.hidden=true
+    document.getElementById("id_bm_y_choice").parentElement.hidden=true
+
+    // has_periodicity checked = true
+    if (document.getElementById("id_has_periodicity").checked == true){
+        document.getElementById("id_periodicity_type").parentElement.hidden=false
+        document.getElementById("id_start").parentElement.hidden=false
+        document.getElementById("id_end").parentElement.hidden=false
+    }
+
+    // By week
+    if (document.getElementById("id_periodicity_type").value == "BW") {
+        document.getElementById("id_bm_x_choice").parentElement.hidden=true
+        document.getElementById("id_bm_y_choice").parentElement.hidden=true
+        document.getElementById("id_bw_weekdays").parentElement.hidden=false
+        document.getElementById("id_bw_weeks_nb").parentElement.hidden=false
+    }
+
+    // Each month at the same date
+    if (document.getElementById("id_periodicity_type").value == "EM") {
+        document.getElementById("id_bw_weekdays").parentElement.hidden=true
+        document.getElementById("id_bw_weeks_nb").parentElement.hidden=true
+        document.getElementById("id_bm_x_choice").parentElement.hidden=true
+        document.getElementById("id_bm_y_choice").parentElement.hidden=true
+    }
+
+    // By month
+    if (document.getElementById("id_periodicity_type").value == "BM") {
+        document.getElementById("id_bw_weekdays").parentElement.hidden=true
+        document.getElementById("id_bw_weeks_nb").parentElement.hidden=true
+        document.getElementById("id_bm_x_choice").parentElement.hidden=false
+        document.getElementById("id_bm_y_choice").parentElement.hidden=false
+    }
+
+    // has_periodicity checked = false
+    if (document.getElementById("id_has_periodicity").checked == false){
+        document.getElementById("id_periodicity_type").parentElement.hidden=true
+        document.getElementById("id_start").parentElement.hidden=true
+        document.getElementById("id_end").parentElement.hidden=true
+        document.getElementById("id_bw_weekdays").parentElement.hidden=true
+        document.getElementById("id_bw_weeks_nb").parentElement.hidden=true
+        document.getElementById("id_bm_x_choice").parentElement.hidden=true
+        document.getElementById("id_bm_y_choice").parentElement.hidden=true
+    }
 }
